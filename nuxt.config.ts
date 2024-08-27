1cc4111a5ead3931bfa33469af4a9f2ed6a8df11
Fatih Aziz <m.fatihalaziz@gmail.com>
2024-08-28 01:24:36 +0700

    feat: Add build step and modify Nuxt config
    - Added a new build step in the GitHub workflow
    - Changed the command for static HTML export with Nuxt
    - Removed unnecessary comment from nuxt.config.ts
    - Ensured correct paths in CSS configuration of nuxt.config.ts
    - Minor formatting changes in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 import { getAllFontCssFiles } from "./build/import_fonts"
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: [
-    '~/assets/css/main.css',
-    ...getAllFontCssFiles()
+    '~/assets/css/main.css',   // Ensure this path is correct and the file exists
+    ...getAllFontCssFiles()    // Ensure this function returns correct paths
   ],
   modules: [
     '@nuxtjs/tailwindcss',
@@ -16,7 +15,7 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-    },
+    }
   },
   ssr: false,
   plugins: [],
