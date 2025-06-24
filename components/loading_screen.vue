fdafdb6888e7323c38482ca3005280162ef03d42
Fatih Aziz <m.fatihalaziz@gmail.com>
2025-06-25 00:34:08 +0700

    Adds loading screen with parallax effects
    - Implements a visually engaging loading screen to enhance user experience.
    - Features a sky gradient background with layered parallax clouds.
    - Includes a village portal animation and dynamic loading messages.
    - Displays a progress bar to indicate loading completion.
    - Transitions smoothly in and out using a fade-slide animation.

--- components/loading_screen.vue
+++ components/loading_screen.vue
@@ -5,18 +5,60 @@
       <div class="sky-gradient"></div>
       
       <!-- Parallax Cloud Layers -->
+      <!-- Background tiny clouds -->
+      <div class="cloud-layer cloud-layer-bg">
+        <div class="cloud tiny-cloud tiny-cloud-1"></div>
+        <div class="cloud tiny-cloud tiny-cloud-2"></div>
+        <div class="cloud tiny-cloud tiny-cloud-3"></div>
+        <div class="cloud tiny-cloud tiny-cloud-4"></div>
+        <div class="cloud tiny-cloud tiny-cloud-5"></div>
+        <div class="cloud tiny-cloud tiny-cloud-6"></div>
+      </div>
+      
+      <!-- Far background clouds -->
       <div class="cloud-layer cloud-layer-1">
         <div class="cloud cloud-1"></div>
         <div class="cloud cloud-2"></div>
         <div class="cloud cloud-3"></div>
       </div>
+      
+      <!-- Big chunky cloud formations behind logo -->
+      <div class="cloud-layer cloud-layer-chunks">
+        <!-- Left cloud formation - multiple clouds stacked -->
+        <div class="cloud-formation cloud-formation-left">
+          <div class="cloud chunky-cloud chunky-left-1"></div>
+          <div class="cloud chunky-cloud chunky-left-2"></div>
+          <div class="cloud chunky-cloud chunky-left-3"></div>
+          <div class="cloud chunky-cloud chunky-left-4"></div>
+          <div class="cloud chunky-cloud chunky-left-5"></div>
+        </div>
+        
+        <!-- Right cloud formation - multiple clouds stacked -->
+        <div class="cloud-formation cloud-formation-right">
+          <div class="cloud chunky-cloud chunky-right-1"></div>
+          <div class="cloud chunky-cloud chunky-right-2"></div>
+          <div class="cloud chunky-cloud chunky-right-3"></div>
+          <div class="cloud chunky-cloud chunky-right-4"></div>
+          <div class="cloud chunky-cloud chunky-right-5"></div>
+        </div>
+        
+        <!-- Top scattered clouds -->
+        <div class="cloud chunky-cloud chunky-top-1"></div>
+        <div class="cloud chunky-cloud chunky-top-2"></div>
+      </div>
+      
+      <!-- Mid-layer clouds -->
       <div class="cloud-layer cloud-layer-2">
         <div class="cloud cloud-4"></div>
         <div class="cloud cloud-5"></div>
       </div>
+      
+      <!-- Foreground clouds -->
       <div class="cloud-layer cloud-layer-3">
         <div class="cloud cloud-6"></div>
         <div class="cloud cloud-7"></div>
+        <div class="cloud cloud-8"></div>
+        <div class="cloud cloud-9"></div>
       </div>
       
       <!-- Loading Content -->
@@ -130,9 +172,11 @@ onMounted(() => {
   left: 0;
 }
 
+.cloud-layer-bg { z-index: 1; }
 .cloud-layer-1 { z-index: 2; }
-.cloud-layer-2 { z-index: 3; }
-.cloud-layer-3 { z-index: 4; }
+.cloud-layer-chunks { z-index: 5; }
+.cloud-layer-2 { z-index: 6; }
+.cloud-layer-3 { z-index: 7; }
 
 /* Individual Clouds */
 .cloud {
@@ -152,149 +196,551 @@ onMounted(() => {
 
 /* Cloud 1 - Large background */
 .cloud-1 {
-  width: 120px;
-  height: 40px;
-  top: 20%;
-  left: 10%;
+  width: 280px;
+  height: 90px;
+  top: 15%;
+  left: 5%;
   animation: float-right 25s infinite linear;
 }
 
 .cloud-1::before {
-  width: 50px;
-  height: 50px;
-  top: -25px;
-  left: 10px;
+  width: 120px;
+  height: 120px;
+  top: -60px;
+  left: 40px;
 }
 
 .cloud-1::after {
-  width: 60px;
-  height: 20px;
-  top: -10px;
-  right: 10px;
+  width: 100px;
+  height: 50px;
+  top: -25px;
+  right: 30px;
 }
 
 /* Cloud 2 - Medium background */
 .cloud-2 {
-  width: 80px;
-  height: 30px;
-  top: 40%;
-  right: 20%;
+  width: 180px;
+  height: 70px;
+  top: 65%;
+  right: 10%;
   animation: float-left 20s infinite linear;
 }
 
 .cloud-2::before {
-  width: 40px;
-  height: 40px;
-  top: -20px;
-  left: 15px;
+  width: 80px;
+  height: 80px;
+  top: -40px;
+  left: 30px;
 }
 
 .cloud-2::after {
-  width: 30px;
-  height: 15px;
-  top: -5px;
-  right: 15px;
+  width: 70px;
+  height: 35px;
+  top: -18px;
+  right: 25px;
 }
 
 /* Cloud 3 - Small background */
 .cloud-3 {
-  width: 60px;
-  height: 20px;
-  top: 15%;
-  right: 10%;
+  width: 140px;
+  height: 50px;
+  top: 8%;
+  right: 20%;
   animation: float-right 30s infinite linear;
 }
 
 .cloud-3::before {
-  width: 30px;
-  height: 30px;
-  top: -15px;
-  left: 10px;
+  width: 70px;
+  height: 70px;
+  top: -35px;
+  left: 25px;
+}
+
+.cloud-3::after {
+  width: 50px;
+  height: 25px;
+  top: -12px;
+  right: 20px;
 }
 
 /* Cloud 4 - Medium midground */
 .cloud-4 {
-  width: 100px;
-  height: 35px;
-  top: 60%;
-  left: 15%;
+  width: 220px;
+  height: 80px;
+  top: 75%;
+  left: 8%;
   animation: float-right 18s infinite linear;
 }
 
 .cloud-4::before {
-  width: 45px;
-  height: 45px;
-  top: -22px;
-  left: 12px;
+  width: 100px;
+  height: 100px;
+  top: -50px;
+  left: 30px;
 }
 
 .cloud-4::after {
-  width: 35px;
-  height: 18px;
-  top: -8px;
-  right: 12px;
+  width: 80px;
+  height: 40px;
+  top: -20px;
+  right: 25px;
 }
 
 /* Cloud 5 - Large midground */
 .cloud-5 {
-  width: 140px;
-  height: 45px;
-  top: 25%;
-  right: 15%;
+  width: 320px;
+  height: 110px;
+  top: 50%;
+  right: 5%;
   animation: float-left 22s infinite linear;
 }
 
 .cloud-5::before {
-  width: 60px;
-  height: 60px;
-  top: -30px;
-  left: 20px;
+  width: 140px;
+  height: 140px;
+  top: -70px;
+  left: 50px;
 }
 
 .cloud-5::after {
-  width: 50px;
-  height: 25px;
-  top: -10px;
-  right: 20px;
+  width: 110px;
+  height: 55px;
+  top: -28px;
+  right: 40px;
 }
 
 /* Cloud 6 - Small foreground */
 .cloud-6 {
-  width: 70px;
-  height: 25px;
+  width: 160px;
+  height: 60px;
   top: 70%;
-  left: 25%;
+  left: 20%;
   animation: float-right 15s infinite linear;
 }
 
 .cloud-6::before {
-  width: 35px;
-  height: 35px;
-  top: -18px;
-  left: 8px;
+  width: 80px;
+  height: 80px;
+  top: -40px;
+  left: 20px;
+}
+
+.cloud-6::after {
+  width: 60px;
+  height: 30px;
+  top: -15px;
+  right: 25px;
 }
 
 /* Cloud 7 - Medium foreground */
 .cloud-7 {
-  width: 90px;
-  height: 30px;
-  top: 80%;
-  right: 30%;
+  width: 240px;
+  height: 85px;
+  top: 85%;
+  right: 15%;
   animation: float-left 16s infinite linear;
 }
 
 .cloud-7::before {
-  width: 40px;
-  height: 40px;
-  top: -20px;
-  left: 15px;
+  width: 110px;
+  height: 110px;
+  top: -55px;
+  left: 40px;
 }
 
 .cloud-7::after {
+  width: 90px;
+  height: 45px;
+  top: -22px;
+  right: 35px;
+}
+
+/* Cloud 8 - Additional foreground */
+.cloud-8 {
+  width: 190px;
+  height: 65px;
+  top: 60%;
+  right: 35%;
+  animation: wiggle-float-right 14s infinite ease-in-out;
+}
+
+.cloud-8::before {
+  width: 85px;
+  height: 85px;
+  top: -42px;
+  left: 30px;
+}
+
+.cloud-8::after {
+  width: 70px;
+  height: 35px;
+  top: -18px;
+  right: 25px;
+}
+
+/* Cloud 9 - Additional foreground */
+.cloud-9 {
+  width: 210px;
+  height: 75px;
+  top: 80%;
+  left: 35%;
+  animation: wiggle-float-left 17s infinite ease-in-out;
+}
+
+.cloud-9::before {
+  width: 95px;
+  height: 95px;
+  top: -48px;
+  left: 40px;
+}
+
+.cloud-9::after {
+  width: 80px;
+  height: 40px;
+  top: -20px;
+  right: 35px;
+}
+
+/* Tiny Background Clouds */
+.tiny-cloud {
+  opacity: 0.4;
+  background: rgba(255, 255, 255, 0.6);
+}
+
+.tiny-cloud-1 {
+  width: 25px;
+  height: 10px;
+  top: 10%;
+  left: 25%;
+  animation: tiny-wiggle 35s infinite ease-in-out;
+}
+
+.tiny-cloud-2 {
+  width: 20px;
+  height: 8px;
+  top: 30%;
+  right: 35%;
+  animation: tiny-wiggle-reverse 40s infinite ease-in-out;
+}
+
+.tiny-cloud-3 {
   width: 30px;
-  height: 15px;
-  top: -5px;
-  right: 15px;
+  height: 12px;
+  top: 50%;
+  left: 15%;
+  animation: tiny-wiggle 45s infinite ease-in-out;
+}
+
+.tiny-cloud-4 {
+  width: 18px;
+  height: 7px;
+  top: 20%;
+  right: 15%;
+  animation: tiny-wiggle-reverse 38s infinite ease-in-out;
+}
+
+.tiny-cloud-5 {
+  width: 22px;
+  height: 9px;
+  top: 75%;
+  left: 60%;
+  animation: tiny-wiggle 42s infinite ease-in-out;
+}
+
+.tiny-cloud-6 {
+  width: 28px;
+  height: 11px;
+  top: 85%;
+  right: 25%;
+  animation: tiny-wiggle-reverse 36s infinite ease-in-out;
+}
+
+/* Cloud Formations - Stationary composite clouds */
+.cloud-formation {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.cloud-formation-left {
+  animation: gentle-sway-left 25s infinite ease-in-out;
+}
+
+.cloud-formation-right {
+  animation: gentle-sway-right 30s infinite ease-in-out;
+}
+
+/* Base chunky cloud style */
+.chunky-cloud {
+  background: rgba(220, 220, 220, 0.7);
+  opacity: 0.6;
+}
+
+/* Left Formation Clouds */
+.chunky-left-1 {
+  width: 300px;
+  height: 120px;
+  top: 20%;
+  left: -15%;
+  animation: subtle-wiggle-1 20s infinite ease-in-out;
+}
+
+.chunky-left-1::before {
+  width: 140px;
+  height: 140px;
+  top: -70px;
+  left: 50px;
+  background: rgba(200, 200, 200, 0.8);
+}
+
+.chunky-left-1::after {
+  width: 110px;
+  height: 60px;
+  top: -30px;
+  right: 30px;
+  background: rgba(210, 210, 210, 0.7);
+}
+
+.chunky-left-2 {
+  width: 250px;
+  height: 100px;
+  top: 35%;
+  left: -10%;
+  animation: subtle-wiggle-2 18s infinite ease-in-out;
+}
+
+.chunky-left-2::before {
+  width: 120px;
+  height: 120px;
+  top: -60px;
+  left: 40px;
+  background: rgba(190, 190, 190, 0.8);
+}
+
+.chunky-left-2::after {
+  width: 90px;
+  height: 50px;
+  top: -25px;
+  right: 20px;
+  background: rgba(205, 205, 205, 0.7);
+}
+
+.chunky-left-3 {
+  width: 320px;
+  height: 140px;
+  top: 50%;
+  left: -20%;
+  animation: subtle-wiggle-3 22s infinite ease-in-out;
+}
+
+.chunky-left-3::before {
+  width: 160px;
+  height: 160px;
+  top: -80px;
+  left: 60px;
+  background: rgba(185, 185, 185, 0.8);
+}
+
+.chunky-left-3::after {
+  width: 130px;
+  height: 70px;
+  top: -35px;
+  right: 40px;
+  background: rgba(200, 200, 200, 0.7);
+}
+
+.chunky-left-4 {
+  width: 180px;
+  height: 80px;
+  top: 68%;
+  left: -5%;
+  animation: subtle-wiggle-4 16s infinite ease-in-out;
+}
+
+.chunky-left-4::before {
+  width: 90px;
+  height: 90px;
+  top: -45px;
+  left: 30px;
+  background: rgba(195, 195, 195, 0.7);
+}
+
+.chunky-left-5 {
+  width: 220px;
+  height: 90px;
+  top: 80%;
+  left: -12%;
+  animation: subtle-wiggle-5 24s infinite ease-in-out;
+}
+
+.chunky-left-5::before {
+  width: 110px;
+  height: 110px;
+  top: -55px;
+  left: 40px;
+  background: rgba(180, 180, 180, 0.7);
+}
+
+.chunky-left-5::after {
+  width: 80px;
+  height: 45px;
+  top: -22px;
+  right: 25px;
+  background: rgba(190, 190, 190, 0.6);
+}
+
+/* Right Formation Clouds */
+.chunky-right-1 {
+  width: 280px;
+  height: 110px;
+  top: 15%;
+  right: -12%;
+  animation: subtle-wiggle-6 21s infinite ease-in-out;
+}
+
+.chunky-right-1::before {
+  width: 130px;
+  height: 130px;
+  top: -65px;
+  left: 50px;
+  background: rgba(205, 205, 205, 0.8);
+}
+
+.chunky-right-1::after {
+  width: 100px;
+  height: 55px;
+  top: -28px;
+  right: 30px;
+  background: rgba(215, 215, 215, 0.7);
+}
+
+.chunky-right-2 {
+  width: 350px;
+  height: 150px;
+  top: 30%;
+  right: -18%;
+  animation: subtle-wiggle-7 19s infinite ease-in-out;
+}
+
+.chunky-right-2::before {
+  width: 170px;
+  height: 170px;
+  top: -85px;
+  left: 70px;
+  background: rgba(190, 190, 190, 0.8);
+}
+
+.chunky-right-2::after {
+  width: 140px;
+  height: 80px;
+  top: -40px;
+  right: 50px;
+  background: rgba(200, 200, 200, 0.7);
+}
+
+.chunky-right-3 {
+  width: 300px;
+  height: 130px;
+  top: 50%;
+  right: -15%;
+  animation: subtle-wiggle-8 23s infinite ease-in-out;
+}
+
+.chunky-right-3::before {
+  width: 150px;
+  height: 150px;
+  top: -75px;
+  left: 60px;
+  background: rgba(185, 185, 185, 0.8);
+}
+
+.chunky-right-3::after {
+  width: 120px;
+  height: 65px;
+  top: -32px;
+  right: 40px;
+  background: rgba(195, 195, 195, 0.7);
+}
+
+.chunky-right-4 {
+  width: 240px;
+  height: 100px;
+  top: 70%;
+  right: -8%;
+  animation: subtle-wiggle-9 17s infinite ease-in-out;
+}
+
+.chunky-right-4::before {
+  width: 120px;
+  height: 120px;
+  top: -60px;
+  left: 50px;
+  background: rgba(180, 180, 180, 0.7);
+}
+
+.chunky-right-5 {
+  width: 200px;
+  height: 85px;
+  top: 85%;
+  right: -10%;
+  animation: subtle-wiggle-10 26s infinite ease-in-out;
+}
+
+.chunky-right-5::before {
+  width: 100px;
+  height: 100px;
+  top: -50px;
+  left: 40px;
+  background: rgba(175, 175, 175, 0.7);
+}
+
+.chunky-right-5::after {
+  width: 75px;
+  height: 40px;
+  top: -20px;
+  right: 20px;
+  background: rgba(185, 185, 185, 0.6);
+}
+
+/* Top scattered clouds */
+.chunky-top-1 {
+  width: 200px;
+  height: 80px;
+  top: 8%;
+  left: 25%;
+  animation: subtle-wiggle-11 20s infinite ease-in-out;
+}
+
+.chunky-top-1::before {
+  width: 100px;
+  height: 100px;
+  top: -50px;
+  left: 40px;
+  background: rgba(190, 190, 190, 0.6);
+}
+
+.chunky-top-2 {
+  width: 180px;
+  height: 70px;
+  top: 12%;
+  right: 30%;
+  animation: subtle-wiggle-12 24s infinite ease-in-out;
+}
+
+.chunky-top-2::before {
+  width: 90px;
+  height: 90px;
+  top: -45px;
+  left: 35px;
+  background: rgba(185, 185, 185, 0.6);
+}
+
+.chunky-top-2::after {
+  width: 70px;
+  height: 35px;
+  top: -18px;
+  right: 25px;
+  background: rgba(195, 195, 195, 0.5);
 }
 
 /* Cloud Animations */
@@ -308,10 +754,112 @@ onMounted(() => {
   100% { transform: translateX(-150px); }
 }
 
+/* Wiggling Animations */
+@keyframes wiggle-float-right {
+  0% { 
+    transform: translateX(-150px) translateY(0px); 
+  }
+  25% { 
+    transform: translateX(25vw) translateY(-8px); 
+  }
+  50% { 
+    transform: translateX(50vw) translateY(5px); 
+  }
+  75% { 
+    transform: translateX(75vw) translateY(-3px); 
+  }
+  100% { 
+    transform: translateX(calc(100vw + 150px)) translateY(0px); 
+  }
+}
+
+@keyframes wiggle-float-left {
+  0% { 
+    transform: translateX(calc(100vw + 150px)) translateY(0px); 
+  }
+  25% { 
+    transform: translateX(75vw) translateY(6px); 
+  }
+  50% { 
+    transform: translateX(50vw) translateY(-4px); 
+  }
+  75% { 
+    transform: translateX(25vw) translateY(7px); 
+  }
+  100% { 
+    transform: translateX(-150px) translateY(0px); 
+  }
+}
+
+/* Tiny Cloud Wiggling */
+@keyframes tiny-wiggle {
+  0%, 100% { 
+    transform: translateX(0px) translateY(0px) scale(1); 
+  }
+  25% { 
+    transform: translateX(3px) translateY(-2px) scale(1.05); 
+  }
+  50% { 
+    transform: translateX(-2px) translateY(3px) scale(0.95); 
+  }
+  75% { 
+    transform: translateX(4px) translateY(1px) scale(1.02); 
+  }
+}
+
+@keyframes tiny-wiggle-reverse {
+  0%, 100% { 
+    transform: translateX(0px) translateY(0px) scale(1); 
+  }
+  25% { 
+    transform: translateX(-3px) translateY(2px) scale(0.98); 
+  }
+  50% { 
+    transform: translateX(2px) translateY(-3px) scale(1.03); 
+  }
+  75% { 
+    transform: translateX(-4px) translateY(-1px) scale(0.97); 
+  }
+}
+
+/* Chunky Cloud Wiggling */
+@keyframes chunky-wiggle-left {
+  0%, 100% { 
+    transform: translateX(0px) translateY(0px) scale(1); 
+  }
+  33% { 
+    transform: translateX(-8px) translateY(-5px) scale(1.02); 
+  }
+  66% { 
+    transform: translateX(6px) translateY(4px) scale(0.98); 
+  }
+}
+
+@keyframes chunky-wiggle-right {
+  0%, 100% { 
+    transform: translateX(0px) translateY(0px) scale(1); 
+  }
+  33% { 
+    transform: translateX(8px) translateY(3px) scale(0.97); 
+  }
+  66% { 
+    transform: translateX(-6px) translateY(-4px) scale(1.03); 
+  }
+}
+
+@keyframes chunky-wiggle-top {
+  0%, 100% { 
+    transform: translateX(0px) translateY(0px) scale(1); 
+  }
+  50% { 
+    transform: translateX(0px) translateY(-6px) scale(1.01); 
+  }
+}
+
 /* Loading Content */
 .loading-content {
   position: relative;
-  z-index: 10;
+  z-index: 15;
   display: flex;
   flex-direction: column;
   align-items: center;
